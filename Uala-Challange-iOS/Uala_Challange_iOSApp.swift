358af520cbc88c20327587af854d7d1fa25f5368
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2024-12-29 16:53:21 -0300

    Initial Commit

---
+++ Uala-Challange-iOS/Uala_Challange_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uala_Challange_iOSApp.swift
+//  Uala-Challange-iOS
+//
+//  Created by Jeremias on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Uala_Challange_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
